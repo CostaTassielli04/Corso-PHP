3adfd6a1bbcd17cb1c183df331af6b1ce0cab2cc
Costantino Tassielli <costantino.tassielli.inf@colamonicochiarulli.edu.it>
2023-04-05 12:39:43 +0200

    Separated db-connect

--- sistema-autenticazione-tassielli-costantino/registrazione.php
+++ sistema-autenticazione-tassielli-costantino/registrazione.php
@@ -1,17 +1,8 @@
 <?php
-$host = "localhost";
-$user = "5a";
-$pass = "dbA1dmin5";
-$db = "5a_tassielli-autenticazione";
-
-// connessione al DBMS con il procedimento ad oggetti
-$conn= new mysqli ($host, $user, $pass, $db);
-if($conn->connect_error){
-    die ("<br>Connessione non riuscita " . $conn->connect_error . " " . $conn->connect_errno);
-}else{
+    require 'db-connect.php';
     doRegister();
     $conn->close();
-}
+
 
 function doRegister(){
     if(!isset($_POST['registra'])){
